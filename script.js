719b09d6d6b7070f40398a66ffc78d4f98c2cf9b
Brian Quinde <bdqs_2747@hotmail.com>
2022-12-21 14:43:19 -0500

    preliminar version messages are working

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const options = {
     today: ["to be a sunny day", "rain", "be a regular day", "be a exciting day"],
-    stay:["good news comming", "a frien could betray you","you are in danger"],
+    stay:["good news comming", "a friend could betray you","you are in danger"],
     you: ["hug your parents","eat a lot", "be brave" , "thing about what you're doing"]
 }
 const proper = Object.getOwnPropertyNames(options);
@@ -9,4 +9,19 @@ const randomizer = (propertie) => {
     return m
 }
 const mens = proper[randomizer(proper)]
-console.log(options[mens][randomizer(options[mens])])
+const complete = options[mens][randomizer(options[mens])]
+
+switch (mens) {
+    case 'today':
+      console.log(`Today is going to ${complete}`);
+      break;
+    case 'stay':
+        console.log(`Stay alert ${complete}`)
+        break;
+    case 'you':
+      console.log(`You must ${complete}`);
+      break;
+    default:
+      console.log(`Sorry, we are out of ${expr}.`);
+      break;
+  }
